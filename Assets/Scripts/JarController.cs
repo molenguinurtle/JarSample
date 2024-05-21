80b7f50649efff0781c56d676bddb50f5ea40270
Ant <young.anthonyd@gmail.com>
2024-05-21 02:22:19 +0200

    Progress on camera and animation functionality.

--- Assets/Scripts/JarController.cs
+++ Assets/Scripts/JarController.cs
@@ -7,11 +7,21 @@ using UnityEngine.Serialization;
 
 public class JarController : MonoBehaviour
 {
+    
+    [System.Serializable]
+    public class JarJsonObj
+    {
+        public string character;
+        public string animation;
+    }
     // Add an event for moving characters around the scene
     // Add an event for starting/stopping a recording of the camera view to write to a local mp4 clip -
     // Configure mp4 output dir within inspector
     // NOTE: Local Folder should be a public variable in Editor [DONE]
     public string jsonFilePath = "";
+    private const float CAMERA_OFFSET_Z = -3f;
+    private const float CAMERA_OFFSET_X = -10f;
+    private const float CAMERA_OFFSET_Y = 5f;
     [SerializeField] private Camera _theCamera; //Set in Inspector
     [SerializeField] private Animator _bananaManController, _robotKyleController;
     [SerializeField] private Transform _bananaTransform, _robotTransform;
@@ -34,24 +44,72 @@ public class JarController : MonoBehaviour
         {
             // Continuously monitors a local folder for new json files [DONE]
             // Ingests said json files and deletes after ingestion [DONE]
-            // Camera focuses on character listed in the JSON
-            // Triggers animation state changes for character listed in the JSON between the two animations you chose 
             DirectoryInfo d = new DirectoryInfo(jsonFilePath);
 
             foreach (var file in d.GetFiles("*.json"))
             {
                 _currentJson = File.ReadAllText(file.ToString());
                 File.Delete(file.ToString());
-                //Do something with the currentJSON (JSON Handler)
-                //_bananaManController.SetTrigger();
-                //_bananaManController.ResetTrigger();
-
+                if (!string.IsNullOrEmpty(_currentJson))
+                {
+                    JsonHandler(_currentJson);
+                    break;
+                }
+                Debug.LogWarning($"The File found is empty. FilePath: {file}");
             }
-
             yield return new WaitForSeconds(1f);
         }
         Debug.LogWarning($"Your filePath Directory doesn't exist! Please check your 'filePath' in the Inspector. FilePath: {jsonFilePath}");
         yield return null;
     }
-    
+
+    private void JsonHandler(string theJson)
+    {
+        // Camera focuses on character listed in the JSON
+        // Triggers animation state changes for character listed in the JSON between the two animations you chose[DONE]
+
+        //_bananaManController.SetTrigger();
+        //_bananaManController.ResetTrigger();
+        JarJsonObj myJarObj = JsonUtility.FromJson<JarJsonObj>(theJson);
+
+        if (myJarObj == null)
+        {
+            Debug.LogWarning($"The JSON did not deserialize into a JarJsonObj. Please verify the formatting of JSON. The Json: {theJson}");
+            return;
+        }
+        switch (myJarObj.character)
+        {
+            case "robot_kyle":
+                CameraHandler(_robotTransform);
+                _robotKyleController.SetTrigger(myJarObj.animation);
+                break;
+            case "banana_man":
+                CameraHandler(_bananaTransform);
+                _bananaManController.SetTrigger(myJarObj.animation);
+                break;
+            default:
+                Debug.LogWarning($"The character name in the JarJsonObj does not match any character in the scene! Camera will not change and animations" +
+                                 $" will not play. Character Name: {myJarObj.character}");
+                break;
+        }
+    }
+
+    private void CameraHandler(Transform cameraFocus)
+    {
+         var focusPosition = cameraFocus.position;
+        // //var focusPosition = cameraFocus.localPosition;
+        Transform camTransform = _theCamera.transform;
+        // Vector3 camLocalPos = camTransform.localPosition;
+        _theCamera.transform.LookAt(camTransform.position + cameraFocus.transform.rotation * -Vector3.forward,
+            camTransform.rotation * Vector3.up);
+        camTransform.position = new Vector3(focusPosition.x + CAMERA_OFFSET_X, focusPosition.y+ CAMERA_OFFSET_Y,
+            focusPosition.z + CAMERA_OFFSET_Z);
+        _theCamera.transform.LookAt(cameraFocus.localPosition);
+
+        // camLocalPos = new Vector3(camTransform.localPosition.x, camLocalPos.y + CAMERA_OFFSET_Y,
+        //     camLocalPos.z + CAMERA_OFFSET_Z);
+        // camTransform.localPosition = camLocalPos;
+
+    }
+
 }
