14e0f9d6d84f6e888cf8664783454685b94e5762
Ant <young.anthonyd@gmail.com>
2024-05-22 02:57:18 +0200

    Mr Fantanstic on his death bed, it's the last stretch. Added JarEvent and EventType serializables for movement and camera recording functionality. Implemented character movement from JarEvent

--- Assets/Scripts/JarController.cs
+++ Assets/Scripts/JarController.cs
@@ -13,15 +13,31 @@ public class JarController : MonoBehaviour
     {
         public string character;
         public string animation;
+        public JarEvent jarEvent;
     }
-    // Add an event for moving characters around the scene
-    // Add an event for starting/stopping a recording of the camera view to write to a local mp4 clip -
-    // Configure mp4 output dir within inspector
-    // NOTE: Local Folder should be a public variable in Editor [DONE]
+    
+    [System.Serializable]
+    public struct JarEvent
+    {
+        public EventType eventType;
+        public bool startRecording;
+        public float movePosX;
+        public float movePosY;
+        public float movePosZ;
+    }
+    
+    [System.Serializable]
+    public enum EventType
+    {
+        IgnoreEvent,
+        MoveEvent,
+        RecordEvent
+    }
+
     public string jsonFilePath = "";
     [SerializeField] private Camera _theCamera;
     [SerializeField] private Animator _bananaManController, _robotKyleController;
-    [SerializeField] private Transform _bananaTransform, _robotTransform;
+    [SerializeField] private Transform _bananaCamPoint, _robotCamPoint, _bananaTransform, _robotTransform;
     private Dictionary<string, string> _currentlyPlayingAnimations = new Dictionary<string, string>();
     private string _currentJson;
     private const string ROBOT_KYLE_STRING = "robot_kyle";
@@ -71,19 +87,42 @@ public class JarController : MonoBehaviour
             Debug.LogWarning($"The JSON did not deserialize into a JarJsonObj. Please verify the formatting of JSON. The Json: {theJson}");
             return;
         }
+        // Add an event for moving characters around the scene [DONE]
+        // Add an event for starting/stopping a recording of the camera view to write to a local mp4 clip -
+        // Configure mp4 output dir within inspector
+        
+        //C'est la vie. Todo: We have a lot of repeated logic below. See if we can't consolidate these calls. Either use conditional (? :) or something else. Probably...
+        //  Todo 2: ...easier to maintain in the long run as well. Maybe group all the separate method calls under one big method call that just passes in the proper
+        //   Todo 3: ...params for all the things being called. (i.e., JarJsonEventHandler(transform blah, transform blahCam, string blahStr, etc.)
         switch (myJarObj.character)
         {
             case ROBOT_KYLE_STRING:
                 StopCurrentlyPlayingAnimation(ROBOT_KYLE_STRING, _robotKyleController);
-                CameraHandler(_robotTransform);
+                if (myJarObj.jarEvent.eventType == EventType.MoveEvent)
+                {
+                    MoveCharacter(_robotTransform, myJarObj.jarEvent);
+                }
+                CameraHandler(_robotCamPoint);
                 _robotKyleController.SetBool(myJarObj.animation, true);
                 TrackCurrentlyPlayingAnimation(ROBOT_KYLE_STRING, myJarObj.animation);
+                if (myJarObj.jarEvent.eventType == EventType.RecordEvent)
+                {
+                    //C'est la vie. Function for start/stopping camera recording goes here.
+                }
                 break;
             case BANANA_MAN_STRING:
                 StopCurrentlyPlayingAnimation(BANANA_MAN_STRING, _bananaManController);
-                CameraHandler(_bananaTransform);
+                if (myJarObj.jarEvent.eventType == EventType.MoveEvent)
+                {
+                    MoveCharacter(_bananaTransform, myJarObj.jarEvent);
+                }
+                CameraHandler(_bananaCamPoint);
                 _bananaManController.SetBool(myJarObj.animation, true);
                 TrackCurrentlyPlayingAnimation(BANANA_MAN_STRING, myJarObj.animation);
+                if (myJarObj.jarEvent.eventType == EventType.RecordEvent)
+                {
+                    //C'est la vie. Function for start/stopping camera recording goes here.
+                }
                 break;
             default:
                 Debug.LogWarning($"The character name in the JarJsonObj does not match any character in the scene! Camera will not change and animations" +
@@ -92,6 +131,12 @@ public class JarController : MonoBehaviour
         }
     }
 
+    #region Helper Methods
+    
+    private void MoveCharacter(Transform charTransform, JarEvent moveParams)
+    {
+        charTransform.position = new Vector3(moveParams.movePosX, moveParams.movePosY, moveParams.movePosZ);
+    }
     private void CameraHandler(Transform cameraFocus)
     {
         var movPosition = cameraFocus.position;
@@ -120,5 +165,7 @@ public class JarController : MonoBehaviour
             _currentlyPlayingAnimations[characterName] = animationName;
         }
     }
+    
+    #endregion
 
 }
